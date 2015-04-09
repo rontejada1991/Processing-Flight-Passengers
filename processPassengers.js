29c4dbcf5aac1ad603e5e9a4c7390cf8a5415b28
Ronald Tejada <rontejada91@gmail.com>
2015-04-09 12:13:16 -0400

    Remove passenger function added
    Added a function to remove objects from the passenger array, we can use
    this when we check who has not paid or is on the no fly list. Also added
    more comments to make the code easier to read.

--- processPassengers.js
+++ processPassengers.js
@@ -1,9 +1,9 @@
 // Processes every passenger, making sure
 // that the function being passed returns false for each
 function processPassengers(passengers, testFunction) {
-	document.write("&#8811 <br>");
 	for (var i = 0; i < passengers.length; i++) {
 		if (testFunction(passengers[i])) {
+			removePassenger(passengers, passengers[i]);
 			return false;
 		}
 	}
@@ -12,12 +12,26 @@ function processPassengers(passengers, testFunction) {
 
 // returns true if Dr. Evel is one of the passenger names
 function checkNoFlyList(passenger) {
-	return passenger.name === "Dr. Evel";
+	return passenger.name === "The Joker";
 }
 
 // returns true if the passenger did not pay
 function checkNotPaid(passenger) {
 	return !passenger.paid;
+}
+
+// remove the passenger
+function removePassenger(passengers, passenger) {
+	var removeIndex = passengers.indexOf(passenger);
+	
+	// Check that it is a valid index
+	// Then remove that index from the array
+	if (removeIndex >= 0) {
+		passengers.splice(removeIndex, 1);
+		return true;
+	}
+	// Return false if nothing was removed
+	return false;
 }
 
 // prints the passengers info, whether or not they have paid, and type of ticket
@@ -33,19 +47,18 @@ function printPassenger(passenger) {
 	return false;
 }
 
+// creates a drink and dinner order function tailored to our customer and assigns it to a local variable, then we can keep calling that function
 function serveCustomer(passenger, orderType) {
 	var getDrinkOrderFunction = createDrinkOrder(passenger);
 	var getDinnerOrderFunction = createDinnerOrder(passenger);
 	// get drink order
 	if (orderType === "drink") {
 		getDrinkOrderFunction();
-
 	} 
 	// get dinner order
 	else if (orderType === "dinner") {
 		getDrinkOrderFunction();
 		getDinnerOrderFunction();
-		getDrinkOrderFunction();
 	} 
 	// show the movie
 	else if (orderType === "movie") {
@@ -53,6 +66,8 @@ function serveCustomer(passenger, orderType) {
 	}
 }
 
+// creates a drink order function based on the ticket type of our passenger
+// and returns it
 function createDrinkOrder(passenger) {
 	var orderFunction;
 	
@@ -72,6 +87,8 @@ function createDrinkOrder(passenger) {
 	return orderFunction;
 }
 
+// creates a dinner order function based on the ticket type of our passenger
+// and returns it
 function createDinnerOrder(passenger) {
 	var orderFunction;
 	
@@ -92,36 +109,43 @@ function createDinnerOrder(passenger) {
 	return orderFunction;
 }
 
-function servePassengers(passengers) {
-	document.write("&#8811 <br>");
+// serves every passengers based on what order type the plane is serving currently
+function servePassengers(passengers, orderType) {
+	document.write("&#8811 Serving Passengers ... <br>");
 	for (var i = 0; i < passengers.length; i++) {
-		serveCustomer(passengers[i], "drink");
+		serveCustomer(passengers[i], orderType);
 	}
-	document.write("&#8811 <br>");
-	for (var i = 0; i < passengers.length; i++) {
-		serveCustomer(passengers[i], "dinner");
-	}
-	document.write("&#8811 <br>");
-	for (var i = 0; i < passengers.length; i++) {
-		serveCustomer(passengers[i], "movie");
-	}
-	document.write("&#8811 <br>");
+	document.write("<br>");
 }
 
-var passengers = [ { name: "Jane Doloop", paid: true, ticket: "coach" },
-				   { name: "Dr. Evel", paid: true, ticket: "premium economy" },
-				   { name: "Sue Property", paid: false, ticket: "first class" },
-				   { name: "John Funcall", paid: true, ticket: "coach" } ]			
+// Array object containing our passenger objects
+var passengers = [ { name: "Bruce Wayne", paid: true, ticket: "first class" },
+				   { name: "The Joker", paid: true, ticket: "first class" },
+				   { name: "Eobard Thawne", paid: false, ticket: "first class" },
+				   { name: "Diana Prince", paid: true, ticket: "premium economy" },
+				   { name: "Ronald Tejada", paid: true, ticket: "premium economy" },
+				   { name: "Clark Kent", paid: true, ticket: "coach" } ]
 
+document.write("&#8811 Printing Passengers ...<br>"); 				   
+processPassengers(passengers, printPassenger);
+document.write("<br>");
+	   
 var allCanFly = processPassengers(passengers, checkNoFlyList);
 if (!allCanFly) {
-	document.write("&#8811 The plane can't take off: we have a passenger on the no-fly-list.<br>");
+	document.write("&#8811 Checking who is on the No Fly List ...<br>"); 
+	document.write("The plane can't take off: we have a passenger on the no-fly-list.<br><br>");
 }
 
 var allPaid = processPassengers(passengers, checkNotPaid);
 if (!allPaid) {
-	document.write("&#8811 The plane can't take off: not everyone has paid.<br>");
+	document.write("&#8811 Checking who has paid ...<br>"); 
+	document.write("The plane can't take off: not everyone has paid.<br><br>");
 }
 
+document.write("&#8811 Printing Passengers ...<br>"); 
 processPassengers(passengers, printPassenger);
-servePassengers(passengers);
+document.write("<br>");
+
+servePassengers(passengers, "drink");
+servePassengers(passengers, "dinner");
+servePassengers(passengers, "movie");
